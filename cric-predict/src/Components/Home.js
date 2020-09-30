4d7ee9c8cb608b3066e8ab06620ea378fbae7786
Jawayria Hashmi <jawayriahashmi@gmail.com>
2020-09-30 20:51:50 +0500

    Added rules and home component. Used redirect component in route but still not working

---
+++ cric-predict/src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Stylesheets/App.css'
+
+function HomeComponent() {
+  return (
+    <p>Home</p>
+  );
+}
+
+export default HomeComponent;
---
+++ cric-predict/src/Components/Rules.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Stylesheets/App.css'
+
+function RulesComponent() {
+  return (
+  <p>Rules</p>
+  );
+}
+
+export default RulesComponent;
