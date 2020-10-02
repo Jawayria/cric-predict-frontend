251fbecfa5f47237cb7f31a5d58133ad348bf512
Jawayria Hashmi <jawayriahashmi@gmail.com>
2020-10-02 18:32:37 +0500

    Removed links for bootstrap CDN and used react-bootstrap components instead

--- cric-predict/src/Components/Carousel.js
+++ cric-predict/src/Components/Carousel.js
@@ -4,10 +4,35 @@ import logo from '../logo.png';
 import '../Stylesheets/App.css'
 import slider2 from './slider2.jpg';
 import slider3 from './slider3.jpg';
+import Carousel from 'react-bootstrap/Carousel'
 
-function Carousel() {
+function CarouselComponent() {
   return (
+  <Carousel>
+    <Carousel.Item>
+        <img
+        className="d-block w-100 carousel-image"
+        src={slider2}
+        alt="First slide"
+        />
+        <div class="carousel-content">
+            <p>This is Dummy text. Just for testing purpose.</p>
+        </div>
+    </Carousel.Item>
+    <Carousel.Item>
+        <img
+        className="d-block w-100 carousel-image"
+        src={slider3}
+        alt="Third slide"
+        />
+        <div class="carousel-content">
+            <p>This is Dummy text. Just for testing purpose.</p>
+        </div>
+    </Carousel.Item>
+</Carousel>
+  /*
   <div>
+
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -40,7 +65,8 @@ function Carousel() {
         </a>
     </div>
     </div>
+    */
   );
 }
 
-export default Carousel;
+export default CarouselComponent;
