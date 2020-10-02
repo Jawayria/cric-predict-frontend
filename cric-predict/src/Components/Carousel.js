cbdc9c61ca20b139a9f18646cbf283c62b25e560
Jawayria Hashmi <jawayriahashmi@gmail.com>
2020-10-03 00:34:21 +0500

    Used React-Bootstrap components instead of Bootstrap classes

--- cric-predict/src/Components/Carousel.js
+++ cric-predict/src/Components/Carousel.js
@@ -2,43 +2,33 @@ import React from 'react';
 import '../Stylesheets/App.css'
 import slider2 from './slider2.jpg';
 import slider3 from './slider3.jpg';
+import Carousel from 'react-bootstrap/Carousel'
 
-function Carousel() {
+function CarouselComponent() {
   return (
-  <div>
-    <div id="myCarousel" className="carousel slide" data-ride="carousel">
-        <ol className="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-        </ol>
-
-        <div className="carousel-inner">
-            <div className="item active">
-                <img src={slider2} alt="Chicago" />
-                <div className="carousel-content">
-                    <p>This is Dummy text. Just for testing purpose.</p>
-                </div>
-            </div>
-
-            <div className="item">
-                <img src={slider3} alt="New York" />
-                <div className="carousel-content">
-                    <p>This is Dummy text. Just for testing purpose.</p>
-                </div>
-            </div>
+  <Carousel>
+    <Carousel.Item>
+        <img
+        className="d-block w-100 carousel-image"
+        src={slider2}
+        alt="First slide"
+        />
+        <div class="carousel-content">
+            <p>This is Dummy text. Just for testing purpose.</p>
         </div>
-
-        <a className="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span className="glyphicon glyphicon-chevron-left"></span>
-            <span className="sr-only">Previous</span>
-        </a>
-        <a className="right carousel-control" href="#myCarousel" data-slide="next">
-            <span className="glyphicon glyphicon-chevron-right"></span>
-            <span className="sr-only">Next</span>
-        </a>
-    </div>
-    </div>
+    </Carousel.Item>
+    <Carousel.Item>
+        <img
+        className="d-block w-100 carousel-image"
+        src={slider3}
+        alt="Third slide"
+        />
+        <div class="carousel-content">
+            <p>This is Dummy text. Just for testing purpose.</p>
+        </div>
+    </Carousel.Item>
+</Carousel>
   );
 }
 
-export default Carousel;
+export default CarouselComponent;
--- cric-predict/src/Components/HeaderMenu.js
+++ cric-predict/src/Components/HeaderMenu.js
@@ -3,16 +3,27 @@ import '../Stylesheets/App.css'
 import {BrowserRouter as Router, Link, Route, Switch, Redirect} from 'react-router-dom';
 import LoginSignup from './LoginSignup';
 import App from '../App';
-import 'react-bootstrap';
+import {Navbar, Nav, NavDropdown, Button}  from 'react-bootstrap';
 
 function HeaderMenuComponent() {
   return (
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto Header-menu">
+	    <Nav.Link className= "Header-item">  <Link to='/'> Home </Link></Nav.Link>
+	    <Nav.Link className= "Header-item">  <Link to='/rules'> Rules </Link></Nav.Link>
+	    <Nav.Link className= "Header-item">  <Link to='/leagues'> Leagues </Link></Nav.Link>
+	    <Button><Nav.Link className= "Header-item">  <Link to='/login'> Login | Signup </Link></Nav.Link></Button>
+	</Nav>
+    </Navbar.Collapse>
+
+  /*
         <ul className='navbar-nav Header-menu'>
             <li className='nav-item Header-item'>  <Link to='/'> Home</Link>  </li>
             <li className='nav-item Header-item'>  <Link to='/rules'> Rules</Link>  </li>
             <li className='nav-item Header-item'>  <Link to='/leagues'>Leagues</Link>  </li>
             <li className='nav-item Header-item'>  <Link to='/login' className='btn-info btn-lg'> Login | Signup </Link></li>
         </ul>
+        */
   );
 }
 
