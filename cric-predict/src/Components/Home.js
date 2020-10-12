fc9d141455cef71ab9505cae2710969082ced002
Jawayria Hashmi <jawayriahashmi@gmail.com>
2020-10-12 19:20:04 +0500

    Done with login and signup

--- cric-predict/src/Components/Home.js
+++ cric-predict/src/Components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../Stylesheets/App.css'
 import Carousel from "./Carousel";
-
+import Leagues from './Leagues';
 function HomeComponent() {
     return (
         <div>
             <Carousel/>
+            <Leagues />
             <h2>this is HomePage</h2>
         </div>
     );
---
+++ cric-predict/src/Components/LoginForm.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import '../Stylesheets/App.css';
+import { Row, Col, Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+
+
+class LoginFormComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {username: '', password: ''};
+    }
+
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value, [event.target.password]: event.target.value});
+        console.log(event.target.value);
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:8000/api/user/login/', {"username": this.state.username, "password": this.state.password})
+        .then(res => {
+          window.localStorage.setItem('access_token', res.data['access']);
+        });
+        this.setState ( {
+            username: '',
+            password: ''
+        })
+    };
+
+
+    render(){
+      return (
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group as={Row} controlId="formPlaintext" style={{marginTop: 20+'px'}}>
+                    <Form.Label column sm="4">
+                        Username
+                    </Form.Label>
+                    <Col sm="8">
+                        <Form.Control type="text" value={this.state.username} onChange={this.handleChange} name="username" placeholder="Username" />
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="formPlaintextPassword">
+                    <Form.Label column sm="4">
+                        Password
+                    </Form.Label>
+                    <Col sm="8">
+                        <Form.Control type="password" value={this.state.password} onChange={this.handleChange} name="password" placeholder="Password" />
+                    </Col>
+                </Form.Group>
+
+                <Button variant="primary" type="submit"  className="submit-button">
+                    Submit
+                </Button>
+            </Form>
+
+      );
+    }
+}
+
+export default LoginFormComponent;
