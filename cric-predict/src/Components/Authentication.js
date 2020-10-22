add419cf8bb27a4f76e5a3df9888b35732cbd180
Jawayria Hashmi <jawayriahashmi@gmail.com>
2020-10-22 22:54:32 +0500

    Made some changes as suggested in review

--- cric-predict/src/Components/Authentication.js
+++ cric-predict/src/Components/Authentication.js
@@ -3,25 +3,39 @@ import '../Stylesheets/App.css';
 import Login from './Login.js';
 import Signup from './Signup.js';
 import {Tabs, Tab } from 'react-bootstrap';
+import {Redirect} from 'react-router-dom';
+import jwt from 'jsonwebtoken'
 
 function AuthenticationComponent() {
-  return (
-    <div className='image-background'>
-       <div className="container form-container" >
-        <div className="form">
-        <h1> Lets Get Started! </h1>
-        <Tabs  id="controlled-tab-example">
-          <Tab eventKey="login" title="Login">
-            <Login />
-          </Tab>
-          <Tab eventKey="sign up" title="Sign up">
-             <Signup />
-           </Tab>
-        </Tabs>
-        </div>
-    </div>
-    </div>
-  );
+    if(localStorage.getItem('access_token'))
+    {
+        var isExpired = false;
+        const token = localStorage.getItem('access_token');
+        var decodedToken=jwt.decode(token, {complete: true});
+        var dateNow = new Date();
+
+        if(!(decodedToken.payload.exp < dateNow.getTime()/1000)){
+                return (<Redirect to="/groups" />);
+            }
+    }
+        return (
+            <div className='image-background'>
+                <div className="container form-container" >
+                    <div className="form">
+                        <h1> Lets Get Started! </h1>
+                        <Tabs  id="controlled-tab-example">
+                            <Tab eventKey="login" title="Login">
+                                <Login />
+                            </Tab>
+                            <Tab eventKey="sign up" title="Sign up">
+                                <Signup />
+                            </Tab>
+                        </Tabs>
+                    </div>
+                </div>
+            </div>
+          );
+
 }
 
 export default AuthenticationComponent;
--- cric-predict/src/Components/Groups.js
+++ cric-predict/src/Components/Groups.js
@@ -6,31 +6,39 @@ import PublicGroups from './PublicGroups';
 import UserGroups from './UserGroups';
 import CreateGroup from './CreateGroup';
 import {Redirect} from "react-router-dom";
+import jwt from 'jsonwebtoken'
+
 
 function GroupComponent() {
-      if(!window.localStorage.getItem('access_token')) {
-        return (<Redirect to="/login" />);
-        }
-      else {
-  return (
-    <div className='image-background'>
-       <div className="container group-container" >
-            <div className="background">
-                <h1> Groups </h1>
-                <div className="row">
-                    <div className="col-sm-4">
-                         <CreateGroup />
-	                     <PublicGroups />
-                    </div>
-                    <div className="col-sm-8">
-                          <UserGroups />
+           if(localStorage.getItem('access_token'))
+           {
+                const token = localStorage.getItem('access_token');
+                var decodedToken=jwt.decode(token, {complete: true});
+                var dateNow = new Date();
+
+                if(decodedToken.payload.exp < dateNow.getTime()/1000){
+                    return (<Redirect to="/login" />);
+                }
+           }
+          return (
+            <div className='image-background'>
+               <div className="container group-container" >
+                    <div className="background">
+                        <h1> Groups </h1>
+                        <div className="row">
+                            <div className="col-sm-4">
+                                 <CreateGroup />
+                                 <PublicGroups />
+                            </div>
+                            <div className="col-sm-8">
+                                  <UserGroups />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-  );
-  }
+          );
+
 }
 
 export default GroupComponent;
